9186563ef2a9fef56f108dc8f223c83d10b62a93
HwangHyunJing <bbq2277@naver.com>
2021-06-14 22:28:42 +0900

    3.1 - 2.* Save & Load의 기본적인 파트까지 함

--- CLC_ObjectManagement/Assets/Scripts/Game.cs
+++ CLC_ObjectManagement/Assets/Scripts/Game.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+
 
 public class Game : MonoBehaviour
 {
@@ -13,6 +15,25 @@ public class Game : MonoBehaviour
     // 게임을 새로 시작하는 키 코드
     public KeyCode newGameKey = KeyCode.N;
 
+    // 정보를 저장하는 키 코드
+    public KeyCode saveKey = KeyCode.S;
+
+    // 저장한 정보를 로딩하는 키 코드
+    public KeyCode loadKey = KeyCode.L;
+
+    // 생성한 물체를 저장할 배열
+    List<Transform> objects;
+
+    // transform 정보를 저장할 위치
+    string savePath;
+
+    private void Awake()
+    {
+        objects = new List<Transform>();
+        // persistent Data Path는 'file'이 아니라 'folder' 경로이다
+        savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(createKey))
@@ -24,6 +45,14 @@ public class Game : MonoBehaviour
         else if(Input.GetKeyDown(newGameKey))
         {
             BeginNewGame();
+        }
+        else if(Input.GetKeyDown(saveKey))
+        {
+            Save();
+        }
+        else if(Input.GetKeyDown(loadKey))
+        {
+            Load();
         }
     }
 
@@ -38,10 +67,72 @@ public class Game : MonoBehaviour
         t.localRotation = Random.rotation;
         // Random. Range는 float를 리턴하기 때문에, transform.scale로 쓰려면 Vector를 곱해야 한다
         t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+
+        // 배열에 추가
+        objects.Add(t);
     }
 
+    // 다시 게임을 시작(Clear 용도)
     void BeginNewGame()
     {
-        Debug.Log("New Game Starts");
+        // Debug.Log("New Game Starts");
+        for(int i=0; i < objects.Count; i++)
+        {
+            // 일단 물체를 제거
+            Destroy(objects[i].gameObject);
+        }
+
+        // 배열의 내용은 한번에 정리
+        objects.Clear();
+    }
+
+    void Save()
+    {
+        // 내용 기록을 위한 open
+        using (
+            // savePath 위치에 파일을 생성/기록하며, writer에 이진 방식으로 접근하겠다
+            var writer = new BinaryWriter(File.Open(savePath, FileMode.Create))
+            )
+        {
+            // 우선 원소의 수를 기록
+            writer.Write(objects.Count);
+
+            for(int i=0; i < objects.Count; i++)
+            {
+                Transform t = objects[i];
+                writer.Write(t.localPosition.x);
+                writer.Write(t.localPosition.y);
+                writer.Write(t.localPosition.z);
+            }
+        }
+        
+    }
+
+    void Load()
+    {
+        // 일단 기본적으로 새 게임을 시작해준다
+        BeginNewGame();
+
+        using(
+            var reader = new BinaryReader(File.Open(savePath, FileMode.Open))
+            )
+        {
+            // 4바이트만큼을 읽습니다
+            int count = reader.ReadInt32();
+
+            for(int i=0; i<count; i++)
+            {
+                // count와는 다르게, transform의 각 값은 float이므로 Read Single을 사용
+
+                Vector3 p;
+                p.x = reader.ReadSingle();
+                p.y = reader.ReadSingle();
+                p.z = reader.ReadSingle();
+
+                Transform t = Instantiate(prefab);
+                t.localPosition = p;
+                objects.Add(t);
+            }
+        }
     }
 }

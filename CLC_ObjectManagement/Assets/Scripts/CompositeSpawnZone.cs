9f503179f95c9650b54404ce4ca9adcf4f0029c0
HwangHyunJing <bbq2277@naver.com>
2021-07-12 15:39:27 +0900

    3.5 : 3.* - 탈출! 은 아니고 사실 물체 크기 오류 수정해야 함

---
+++ CLC_ObjectManagement/Assets/Scripts/CompositeSpawnZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompositeSpawnZone : SpawnZone
+{
+    // 한 스크립트로 복수의 구역들을 생성
+    [SerializeField]
+    SpawnZone[] spawnZones;
+
+    public override Vector3 SpawnPoint
+    {
+        get
+        {
+            // 여러 묶음 중 한 구획을 랜덤으로 지정
+            int index = Random.Range(0, spawnZones.Length);
+            return spawnZones[index].SpawnPoint;
+        }
+    }
+}

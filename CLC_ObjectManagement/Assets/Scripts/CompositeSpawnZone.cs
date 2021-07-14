caa2a1ced2895c7cbc392b4fbe02964205e05d15
HwangHyunJing <bbq2277@naver.com>
2021-07-14 20:32:59 +0900

    3.6 : 3.2 - 3.3은 도저히 그 철학을 모르겠음. 뭘 자꾸 저장하려는 거야?

--- CLC_ObjectManagement/Assets/Scripts/CompositeSpawnZone.cs
+++ CLC_ObjectManagement/Assets/Scripts/CompositeSpawnZone.cs
@@ -8,13 +8,47 @@ public class CompositeSpawnZone : SpawnZone
     [SerializeField]
     SpawnZone[] spawnZones;
 
+    // 배정된 각 공간을 '순서대로' 돌며 생성할지 여부
+    [SerializeField]
+    bool sequential;
+
+    int nextSequentialIndex;
+
     public override Vector3 SpawnPoint
     {
         get
         {
             // 여러 묶음 중 한 구획을 랜덤으로 지정
-            int index = Random.Range(0, spawnZones.Length);
+            int index;
+
+            if(sequential)
+            {
+                // 시퀀셜이 켜져있다면, 무조건 다음 순서의 index를 가져온다
+                index = nextSequentialIndex++;
+
+                // 인덱스가 끝까지 다다랐다면 다시 처음으로 돌린다
+                if(nextSequentialIndex >= spawnZones.Length)
+                {
+                    // 우리가 사용할 값은 index이므로, 0이 되는 건 next Sequential Index가 맞다
+                    nextSequentialIndex = 0;
+                }
+            }
+            else
+            {
+                index = Random.Range(0, spawnZones.Length);
+            }
+
             return spawnZones[index].SpawnPoint;
         }
+    }
+
+    public override void Save(GameDataWriter writer)
+    {
+        writer.Write(nextSequentialIndex);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        nextSequentialIndex = reader.ReadInt();
     }
 }
--- CLC_ObjectManagement/Assets/Scripts/SpawnZone.cs
+++ CLC_ObjectManagement/Assets/Scripts/SpawnZone.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // abstract 클래스는 자신의 객체를 생성할 수 없습니다
-public abstract class SpawnZone : MonoBehaviour
+public abstract class SpawnZone : PersistableObject
 {
+    // zone에 대한 정보 - 가령 next sequential index 라던가 - 를 저장하기 위해 P.O.를 상속
+
     public abstract Vector3 SpawnPoint { get; }
 
+    
 }

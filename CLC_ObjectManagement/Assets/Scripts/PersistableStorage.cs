e544df76cd8a2b72ba210c135d53ea5aede3362e
HwangHyunJing <bbq2277@naver.com>
2021-07-14 19:45:03 +0900

    3.6 : 2.* - 레벨 데이터에 대한 저장 및 정리까지 해결

--- CLC_ObjectManagement/Assets/Scripts/PersistableStorage.cs
+++ CLC_ObjectManagement/Assets/Scripts/PersistableStorage.cs
@@ -26,11 +26,18 @@ public class PersistableStorage : MonoBehaviour
 
     public void Load(PersistableObject o)
     {
+        /*
         using(
             var reader = new BinaryReader(File.Open(savePath, FileMode.Open))
             )
         {
             o.Load(new GameDataReader(reader, -reader.ReadInt32()));
-        }
+        }*/
+
+        // 코루틴으로 인해 파일이 조기에 닫히는 문제로, 여는 방식을 변경
+        byte[] data = File.ReadAllBytes(savePath);
+        // Binary류 메소드는 단순 배열인 data(byte[])를 읽을 수 없으므로 스트림으로 전환
+        var reader = new BinaryReader(new MemoryStream(data));
+        o.Load(new GameDataReader(reader, -reader.ReadInt32()));
     }
 }
